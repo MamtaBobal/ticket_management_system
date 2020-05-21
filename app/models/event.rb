b46bb607f597d25b46051e83dd7da92f9b00547d
MamtaBobal <mamta.cs.du.2013@gmail.com>
2020-05-21 14:22:19 +0530

    5% discount on the ticket if gender is female

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,6 +7,12 @@ class Event < ApplicationRecord
   has_many :user_events
   has_many :users, through: :user_events
 
+  # get event fee according to gender of the user
+  def get_ticket_fee(user)
+    self.fee = self.fee - 0.05 * self.fee if user.gender == "female"
+    return self.fee
+  end
+
   # verify past event or not
   def is_past_event?
     self.event_date.localtime < DateTime.now
